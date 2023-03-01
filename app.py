a1636707b180409100c8202b63976d46a1d25896
hojaeklee <hojaelee@umich.edu>
2023-03-01 10:24:13 -0500

    Add a simple app

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("2D Neuronal Morphology Prediction")
+st.write("This web tool predicts the neuronal morphology for a given scRNAseq data based on the paper [MorphNet Predicts Cell Morphology from Single-Cell Gene Expression](https://www.biorxiv.org/content/10.1101/2022.10.21.513201v1) by Hojae Lee and Joshua D. Welch.")
